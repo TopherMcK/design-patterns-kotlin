3bcad1892e2f9738fb5d3cb0fe31d9ba94861294
Christopher McKinney <123topher456@gmail.com>
2019-06-23 13:36:14 -0500

    added robolectric test for LandingActivity

--- app/src/main/java/com/clm/designpatternskotlin/DesignPatternsKotlinApplication.kt
+++ app/src/main/java/com/clm/designpatternskotlin/DesignPatternsKotlinApplication.kt
@@ -3,23 +3,24 @@ package com.clm.designpatternskotlin
 import android.app.Application
 import com.clm.designpatternskotlin.di.utilModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext
 import org.koin.core.context.startKoin
 import timber.log.Timber
-import java.util.Collections.singletonList
 
 class DesignPatternsKotlinApplication : Application() {
 
     override fun onCreate(){
         super.onCreate()
 
-        val context = this
+        if(GlobalContext.getOrNull() == null) {
+            val context = this
+            startKoin {
+                modules(
+                    listOf(utilModule)
+                ).androidContext(context)
+            }
 
-        startKoin {
-            modules(
-                singletonList(utilModule)
-            ).androidContext(context)
+            Timber.plant(Timber.DebugTree())
         }
-
-        Timber.plant(Timber.DebugTree())
     }
 }
---
+++ app/src/test/java/com/clm/designpatternskotlin/landing/LandingActivityTest.kt
@@ -0,0 +1,44 @@
+package com.clm.designpatternskotlin.landing
+
+import com.clm.designpatternskotlin.R
+import com.clm.designpatternskotlin.ducks.ui.StrategyActivity
+import kotlinx.android.synthetic.main.content_landing.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows.shadowOf
+
+@RunWith(RobolectricTestRunner::class)
+class LandingActivityTest {
+    private lateinit var landingActivity: LandingActivity
+
+    @Before
+    fun setup() {
+        landingActivity = Robolectric
+            .buildActivity(LandingActivity::class.java)
+            .create()
+            .resume()
+            .get()
+    }
+
+    @Test
+    fun whenStrategyButtonIsPressedThenStartsStrategyActivity(){
+        var expectedLabel = landingActivity.getString(R.string.strategy_label)
+        var actualLabel = landingActivity.duckBtn.text.toString()
+        assertEquals(expectedLabel, actualLabel)
+        
+        landingActivity.duckBtn.performClick()
+        assertActivityStarted(StrategyActivity::class.java)
+    }
+
+    private fun assertActivityStarted(activityToStart: Class<*>){
+        val shadowActivity = shadowOf(landingActivity)
+        val startedIntent = shadowActivity.nextStartedActivity
+        val shadowIntent = shadowOf(startedIntent)
+        assertEquals(activityToStart.name, shadowIntent.intentClass.name)
+    }
+}
